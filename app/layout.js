df5a5c18fddcbd9f0daae7fec85e968906006ab3
Usama Azam <usamaiskills@gmail.com>
2026-06-02 22:07:13 +0500

    Update whatsapp line layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import WhatsAppFloat from '@/components/WhatsAppFloat';
 
 export const metadata = {
   title: 'Expenditure Generator - KPK Schools',
@@ -8,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* Floating WhatsApp button - shows on ALL pages */}
+        <WhatsAppFloat />
+      </body>
     </html>
   );
 }
